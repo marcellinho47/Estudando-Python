30c746aa041b22dea17085a4b71fd488e6bbba46
Marcello <marcello_doalves@hotmail.com>
2024-09-27 17:57:21 -0300

    improve game score

--- snake/snake.py
+++ snake/snake.py
@@ -1,4 +1,6 @@
 import curses
+import random
+import time
 
 
 def draw_screen(window):
@@ -7,10 +9,32 @@ def draw_screen(window):
 
 
 def draw_snake(window, snake):
-    window.addch(snake[0], snake[1], curses.ACS_CKBOARD)
+    head = snake[0]
+    draw_in_screen(window, head, '@')
+    for part in snake[1:]:
+        draw_in_screen(window, part, '*')
+
+
+def draw_in_screen(window, actor, char):
+    window.addch(actor[0], actor[1], char)
 
 
 def move_snake(snake, direction):
+    head = snake[0].copy()
+    move_snake_part(head, direction)
+    snake.insert(0, head)
+    snake.pop()
+
+
+def generate_new_fruit(window, snake):
+    height, width = window.getmaxyx()
+    return [
+        random.randint(1, height - 2),
+        random.randint(1, width - 2)
+    ]
+
+
+def move_snake_part(snake, direction):
     match direction:
         case curses.KEY_RIGHT:
             snake[1] += 1
@@ -32,24 +56,76 @@ def get_new_direction(window, timeout):
 
 def check_hit_wall(snake, window):
     height, width = window.getmaxyx()
-    return snake[0] in (0, height - 1) or snake[1] in (0, width - 1)
+    head = snake[0]
+    return head[0] in (0, height - 1) or head[1] in (0, width - 1)
+
+
+def check_hit_fruit(snake, fruit):
+    if snake[0] == fruit:
+        return True
+
+
+def check_hit_snake(snake):
+    return snake[0] in snake[1:]
+
+
+def direction_is_opposite(current_direction, direction):
+    return (current_direction == curses.KEY_RIGHT and direction == curses.KEY_LEFT) or \
+        (current_direction == curses.KEY_LEFT and direction == curses.KEY_RIGHT) or \
+        (current_direction == curses.KEY_UP and direction == curses.KEY_DOWN) or \
+        (current_direction == curses.KEY_DOWN and direction == curses.KEY_UP)
+
+
+def finish_game(window, score):
+    window.clear()
+    window.border(0)
+    height, width = window.getmaxyx()
+    message = f'Game Over! Score: {score}'
+    y = int(height / 2)
+    x = int((width - len(message)) / 2)
+    window.addstr(y, x, message)
+    window.refresh()
+    time.sleep(10)
 
 
 def game_loop(window):
     curses.curs_set(0)
-    snake = [10, 15]
+    snake = [
+        [10, 15],
+        [10, 14]
+    ]
+    fruit = generate_new_fruit(window, snake)
+    current_direction = curses.KEY_RIGHT
+    score = 0
 
     while True:
         draw_screen(window=window)
         draw_snake(window, snake)
+        draw_in_screen(window, fruit, curses.ACS_DIAMOND)
+        direction = get_new_direction(window=window, timeout=1)
 
-        direction = get_new_direction(window=window, timeout=1000)
+        if direction is None:
+            direction = current_direction
 
-        if direction is not None:
-            move_snake(snake=snake, direction=direction)
+        if direction_is_opposite(current_direction, direction):
+            direction = current_direction
+
+        move_snake(snake=snake, direction=direction)
 
         if check_hit_wall(snake=snake, window=window):
-            return
+            break
+
+        if check_hit_snake(snake=snake):
+            break
+
+        if check_hit_fruit(snake=snake, fruit=fruit):
+            fruit = generate_new_fruit(window, snake)
+            snake.append(snake[-1].copy())
+            score += 1
+
+        current_direction = direction
+
+    finish_game(window, score)
 
 
 if __name__ == "__main__":
