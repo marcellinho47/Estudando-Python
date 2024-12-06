d4c03b7be73e310416e1f8e12e365e1c5ce4d511
Marcello <marcellodoalves@hotmail.com>
2024-12-06 14:53:55 -0300

    divisor de arquivos

--- excel-to-sql-server/excel-tabs-to-txt-file.py
+++ excel-to-sql-server/excel-tabs-to-txt-file.py
@@ -10,6 +10,7 @@ def import_excel_to_txt(from_directory, to_directory):
 
             clean_filename = os.path.splitext(filename)[0]
 
+            # CSV Files
             if filename.endswith('.csv'):
                 xls = pd.read_csv(excel_path, dtype=str)
                 sheet_names = [os.path.splitext(filename)[0]]
@@ -24,6 +25,7 @@ def import_excel_to_txt(from_directory, to_directory):
                     df.to_csv(txt_file_path, sep='\t', index=False, encoding='ansi')
                     print(f'Exported {clean_filename}.txt from {filename}')
 
+            # Excel Files
             else:
                 xls = pd.ExcelFile(excel_path)
                 sheet_names = xls.sheet_names
@@ -31,19 +33,21 @@ def import_excel_to_txt(from_directory, to_directory):
                 # Iterate over each sheet
                 for sheet_name in sheet_names:
                     df = pd.read_excel(xls, sheet_name=sheet_name).map(
-                        lambda x: x.replace('\t', '') if isinstance(x, str) else x)
+                        lambda x: x.replace('\t', '') if isinstance(x, str) else x).map(
+                        lambda x: x.replace('\n', '') if isinstance(x, str) else x).map(
+                        lambda x: x.replace('\r\n', '') if isinstance(x, str) else x)
 
                     if df.empty:
-                        print(f'Ignored {clean_filename}.txt from {filename}')
+                        print(f'Ignored {sheet_name}.txt from {sheet_name}')
                         continue
                     else:
                         # Export the DataFrame to a delimited txt file
-                        txt_file_path = os.path.join(to_directory, f'{clean_filename}.txt')
+                        txt_file_path = os.path.join(to_directory, f'{sheet_name}.txt')
                         df.to_csv(txt_file_path, sep='\t', index=False, encoding='ansi')
-                        print(f'Exported {clean_filename}.txt from {filename}')
+                        print(f'Exported {sheet_name}.txt from {sheet_name}')
 
 
 # Usage
-from_directory = 'C:\\Users\\marcello.alves\\Downloads\\Anton Paar'
-to_directory = 'C:\\Users\\marcello.alves\\Downloads\\Anton Paar TXT'
+from_directory = 'C:\\Users\\marce\\Downloads'
+to_directory = 'C:\\Users\\marce\\Downloads'
 import_excel_to_txt(from_directory, to_directory)
